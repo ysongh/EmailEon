391dfd6dee72230c8c27082c27da811bc59d547b
You Song Hou <ysongweb3@gmail.com>
2024-04-12 21:28:33 -0400

    feat: display the list of store Id

--- packages/nextjs/app/dashboard/page.tsx
+++ packages/nextjs/app/dashboard/page.tsx
@@ -2,17 +2,48 @@
 
 import type { NextPage } from "next";
 import Link from 'next/link';
+import { useEffect, useState } from "react";
 import { useAccount } from "wagmi";
 import { useScaffoldContractRead } from "~~/hooks/scaffold-eth";
+import { retrieveSecretBlob } from "~~/utils/nillion/retrieveSecretBlob";
 
 const Dashboard: NextPage = () => {
   const { address } = useAccount();
+
+  const [userKey, setUserKey] = useState<string | null>(null);
+  const [nillion, setNillion] = useState<any>(null);
+  const [userId, setUserId] = useState<string | null>(null);
+  const [nillionClient, setNillionClient] = useState<any>(null);
+  const [retrievedValue, setRetrievedValue] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (userKey) {
+      const getNillionClientLibrary = async () => {
+        const nillionClientUtil = await import("~~/utils/nillion/nillionClient");
+        const libraries = await nillionClientUtil.getNillionClient(userKey);
+
+        setNillion(libraries.nillion);
+        setNillionClient(libraries.nillionClient);
+
+        return libraries.nillionClient;
+      };
+
+      getNillionClientLibrary().then(nillionClient => {
+        const user_id = nillionClient.user_id;
+        setUserId(user_id);
+      });
+    }
+  }, [userKey]);
 
   const { data: emails } = useScaffoldContractRead({
     contractName: "EmailEon",
     functionName: "getEmails",
     args: [address],
   });
+
+  async function handleRetrieveSecretBlob(store_id: string, secret_name: string) {
+    await retrieveSecretBlob(nillionClient, store_id, secret_name).then(setRetrievedValue);
+  }
 
   return (
     <div className="flex h-screen bg-gray-100">
@@ -30,6 +61,9 @@ const Dashboard: NextPage = () => {
         </header>
         <main className="container mx-auto py-6 px-4">
           <p>{emails?.length} subscribers</p>
+          {emails?.map((e, index) => (
+            <p key={index}>{e.secretName} {e.storeId}</p>
+          ))}
         </main>
       </div>
     </div>
