c6c8e7c14c0f81ac5be33a1ac77bb579a1f003b8
You Song Hou <ysongweb3@gmail.com>
2024-04-13 17:45:26 -0400

    feat: integrate Mailchain to send mail via an API

---
+++ packages/nextjs/app/api/email/send/route.tsx
@@ -0,0 +1,27 @@
+import { Mailchain } from '@mailchain/sdk';
+
+const GET = async() => {
+  const secretRecoveryPhrase = process.env.SECRET_RECOVERY_PHRASE!; // 25 word mnemonicPhrase
+
+  const mailchain = Mailchain.fromSecretRecoveryPhrase(secretRecoveryPhrase);
+
+  const { data, error } = await mailchain.sendMail({
+      from: `emaileon@mailchain.com`, // sender address
+      to: [``], // list of recipients (blockchain or mailchain addresses)
+      subject: 'My first message', // subject line
+      content: {
+          text: 'Hello Mailchain 👋', // plain text body
+          html: '<p>Hello Mailchain 👋</p>', // html body
+      },
+  });
+  if (error) {
+      // handle error
+      console.warn('Mailchain error', error);
+      return;
+  }
+  // handle success send mail result
+  console.log(data);
+  return Response.json({ data: data });
+  }
+
+export { GET };
