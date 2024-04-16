76617c3a463578a0118a89f2eea7f2f7f09de1e5
You Song Hou <ysongweb3@gmail.com>
2024-04-15 21:40:17 -0400

    feat: display Nillion User ID on the profile page

--- packages/nextjs/app/profile/page.tsx
+++ packages/nextjs/app/profile/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
+import { CopyString } from "~~/components/nillion/CopyString";
 import { useScaffoldContractRead, useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
+import { getUserKeyFromSnap } from "~~/utils/nillion/getUserKeyFromSnap";
 
 const Profile: NextPage = () => {
   const { address: connectedAddress } = useAccount();
@@ -14,8 +16,32 @@ const Profile: NextPage = () => {
     args: [connectedAddress],
   });
 
+  const [nillion, setNillion] = useState<any>(null);
+  const [nillionClient, setNillionClient] = useState<any>(null);
   const [email, setEmail] = useState(profile?.email || '');
   const [isEditing, setIsEditing] = useState(false);
+  const [connectedToSnap, setConnectedToSnap] = useState<boolean>(false);
+  const [userId, setUserId] = useState<string | null>(null);
+  const [userKey, setUserKey] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (userKey) {
+      const getNillionClientLibrary = async () => {
+        const nillionClientUtil = await import("~~/utils/nillion/nillionClient");
+        const libraries = await nillionClientUtil.getNillionClient(userKey);
+
+        setNillion(libraries.nillion);
+        setNillionClient(libraries.nillionClient);
+
+        return libraries.nillionClient;
+      };
+
+      getNillionClientLibrary().then(nillionClient => {
+        const user_id = nillionClient.user_id;
+        setUserId(user_id);
+      });
+    }
+  }, [userKey]);
 
   const { writeAsync: createProfile} = useScaffoldContractWrite({
     contractName: "EmailEon",
@@ -26,6 +52,12 @@ const Profile: NextPage = () => {
       console.log("Transaction blockHash", txnReceipt.blockHash);
     },
   });
+
+  async function handleConnectToSnap() {
+    const snapResponse = await getUserKeyFromSnap();
+    setUserKey(snapResponse?.user_key || null);
+    setConnectedToSnap(snapResponse?.connectedToSnap || false);
+  }
 
   const handleEditClick = () => {
     setIsEditing(true);
@@ -41,8 +73,26 @@ const Profile: NextPage = () => {
       <div className="max-w-4xl mx-auto py-8 px-4">
         <h1 className="text-3xl font-semibold mb-4">Profile</h1>
         <div className="bg-white shadow-md rounded-lg p-6 w-[500px]">
-         
-          <div className="mb-4">
+          {!connectedAddress && <p>Connect your MetaMask Flask wallet</p>}
+          {connectedAddress && connectedToSnap && !userKey && (
+            <a target="_blank" href="https://nillion-snap-site.vercel.app/" rel="noopener noreferrer">
+              <button className="btn btn-sm btn-primary mt-4">
+                No Nillion User Key - Generate and store user key here
+              </button>
+            </a>
+          )}
+          {connectedAddress && !connectedToSnap && (
+            <button className="btn btn-sm btn-primary mt-4" onClick={handleConnectToSnap}>
+              Connect to Snap with your Nillion User Key
+            </button>
+          )}
+          {userId && (
+            <div className="flex items-center space-x-2">
+              <p className="my-2 font-medium">Connected as Nillion User ID:</p>
+              <CopyString str={userId} />
+            </div>
+          )}
+          <div className="mt-5 mb-4">
             <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email:</label>
             {isEditing ? (
               <input 
